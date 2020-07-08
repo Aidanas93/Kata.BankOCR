4edfe0f29c3d19593787e1d752cafe315de3c411
Aidanas93 <aidanas93@gmail.com>
2020-07-08 16:57:31 +0300

    Added one line parser

--- Kata.BankOCR/Program.cs
+++ Kata.BankOCR/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Kata.BankOCR
 {
@@ -6,7 +7,50 @@ namespace Kata.BankOCR
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] text = System.IO.File.ReadAllLines(@"C:\Users\Aidanas\Desktop\Kata BankOCR\Kata.BankOCR\BankOCR.txt");
+
+            IList<string[,]> numbers = new List<string[,]>();
+
+            for(int i = 0; i < 9; i++)
+            {
+                numbers.Add(new string[3, 3]
+                {{" "," "," "},
+                 {" "," "," "},
+                 {" "," "," "} 
+                });
+                // j row
+                for (int j = 0; j < 3; j++)
+                {
+                    // k column
+                    int x = i * 3 + 3;
+                    for (int k = i*3; k < x; k++)
+                    {
+                        string textRow;
+                        string textChar;
+                        textRow = text[j];
+
+
+                        textChar = textRow.Substring(k, 1);
+                        numbers[i][j, k - (i*3)] = textChar;
+                    }
+                }
+            }
+
+
+
+
+
+            string[,] numberOne = new string[3, 3]
+            {{" "," "," "},
+             {" "," ","|"},
+             {" "," ","|"} };
+
+            string[,] numberTwo = new string[3, 3]
+            {{" ","_"," "},
+             {" ","_","|"},
+             {"|","_"," "} };
+
+            numbers.Add(numberOne);
         }
     }
 }
